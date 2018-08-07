35c2d5bf91afb76aae91b12d51bb9035debda7fa
unknown <matthew.teja@gmail.com>
2018-08-07 12:37:25 -0700

    Added gameActivity activity, made startGame start the game actibity, changed AppTheme

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/MainActivity.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gerrymatthewnick.randomsideproject.tempfishingname;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void changeToGame(View view) {
+        Intent intent = new Intent(this, gameActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
 }
