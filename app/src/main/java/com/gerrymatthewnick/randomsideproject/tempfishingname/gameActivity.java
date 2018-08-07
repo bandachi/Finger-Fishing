ce6432a8e0051cbbc27ce8bff1b0fbee2f123938
unknown <matthew.teja@gmail.com>
2018-08-07 14:43:39 -0700

    Added onTouchEvent for fishing line to follow finger

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/gameActivity.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/gameActivity.java
@@ -1,13 +1,48 @@
 package com.gerrymatthewnick.randomsideproject.tempfishingname;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
 public class gameActivity extends AppCompatActivity {
+
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        img = findViewById(R.id.fishingLine);
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                img.setX(x);
+                img.setY(y);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                img.setX(x);
+                img.setY(y);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
+
+            default:
+                break;
+        }
+        return false;
+    }
+
+
+
+
 }
