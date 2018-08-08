47502313816edd774978997d798cd23bc8aab37e
unknown <matthew.teja@gmail.com>
2018-08-08 13:01:28 -0700

    Cleaned up code

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/Fish.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/Fish.java
@@ -4,29 +4,62 @@ import android.content.Context;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.util.logging.Handler;
+
 public class Fish {
 
     private int type;
     private int changeFreq;
     private RelativeLayout rl;
     private static Context context;
+    private int maxVel;
+    private float velX;
+    private float velY;
+    private android.os.Handler changeVel;
+
+
 
     private ImageView fish;
 
-    public Fish(int fishType, int change, RelativeLayout relativeL, Context current) {
+    public Fish(int fishType, int change, int vel, RelativeLayout relativeL, Context current, android.os.Handler CHANGE_FISH_VELOCITY) {
 
         type = fishType;
         changeFreq = change;
+        maxVel = vel;
         rl = relativeL;
         context = current;
+        changeVel = CHANGE_FISH_VELOCITY;
+
 
     }
+
+
+
     public void setX(float x) {
         fish.setX(x);
 
     }
     public void setY(float y) {
         fish.setY(y);
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                velX = (float)(Math.random() * maxVel);
+                velY = (float)(Math.random() * maxVel);
+                fish.setX(velX);
+                fish.setY(velY);
+            }
+            finally {
+                changeVel.postDelayed(runnable, changeFreq);
+            }
+        }
+    };
+
+    public void startChangeVelocity() {
+        runnable.run();
     }
 
 
--- app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/gameActivity.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/gameActivity.java
@@ -14,8 +14,7 @@ public class gameActivity extends AppCompatActivity {
     private RelativeLayout rl;
     private ImageView line;
     final Handler START_SPAWN_FISH = new Handler();
-    final Handler CHANGE_FISH_VELOCITY = new Handler();
-    final Handler FISH_VELOCITY = new Handler();
+    Handler CHANGE_FISH_VELOCITY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,67 +24,28 @@ public class gameActivity extends AppCompatActivity {
         rl = findViewById(R.id.rlGame);
         line = findViewById(R.id.fishingLine);
 
+
         //Start fish spawning method after 3 seconds
         START_SPAWN_FISH.postDelayed(new Runnable() {
             @Override
             public void run() {
-                //spawnFish();
+                CHANGE_FISH_VELOCITY = new Handler();
+
                 int id = getResources().getIdentifier("fish1" , "drawable", getPackageName());
-                Fish fish = new Fish(id, 1000, rl, getApplicationContext());
+
+
+                Fish fish = new Fish(id, 1000, 10, rl, getApplicationContext(), CHANGE_FISH_VELOCITY);
                 fish.spawnFish();
                 fish.setX(300);
                 fish.setY(300);
+
+                fish.startChangeVelocity();
             }
         }, 3000);
 
     }
 
-    //repeats in CHANGE intervals, updating moveFish();
-    public class changeVelocity implements Runnable {
 
-        private ImageView fish;
-        final int CHANGE = 1000;
-        public changeVelocity(ImageView img) {
-            this.fish = img;
-        }
-
-        @Override
-        public void run() {
-            try {
-                moveFish(fish);
-            } finally {
-                CHANGE_FISH_VELOCITY.postDelayed(this, CHANGE);
-            }
-        }
-    }
-
-    public void spawnFish() {
-
-        //Add fish to layout
-        final ImageView fish = new ImageView(this);
-        fish.setImageResource(R.drawable.fish1);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        fish.setLayoutParams(lp);
-        rl.addView(fish);
-
-        fish.setX(300);
-        fish.setY(300);
-
-        //change fish's velocity
-        changeVelocity obj = new changeVelocity(fish);
-        obj.run();
-
-    }
-
-
-    //set direction and speed of fish
-    public void moveFish(ImageView img) {
-        int velocityX = (int)(Math.random() * 4);
-        int velocityY = (int)(Math.random() * 5);
-
-        img.setX(velocityX);
-        img.setY(velocityY);
-    }
     //keep fishing line image on screen touch
     @Override
     public boolean onTouchEvent(MotionEvent event) {
