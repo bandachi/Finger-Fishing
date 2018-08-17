d0a7f55de0954fffecae6b700f013b5a9818f392
unknown <matthew.teja@gmail.com>
2018-08-16 18:09:30 -0700

    Added current coin count to shop activity

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/ShopActivity.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/tempfishingname/ShopActivity.java
@@ -1,13 +1,23 @@
 package com.gerrymatthewnick.randomsideproject.tempfishingname;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShopActivity extends AppCompatActivity {
 
+    public int coinsShop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        SharedPreferences settingsCoins = getSharedPreferences("coins", MODE_PRIVATE);
+        coinsShop = settingsCoins.getInt("coinCount", 0);
+
+        TextView coinDisplay = findViewById(R.id.coinDisplayShop);
+        coinDisplay.setText(coinDisplay.getText().toString() + Integer.toString(coinsShop));
+        //all yours coinsShop is the amount of coins user has
     }
 }
